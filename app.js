289d941f5615f84e4ab7074373d327e03fd71158
pharm2data <93396344+pharm2data@users.noreply.github.com>
2024-04-08 20:20:36 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const reviews = [
+{
+id: 1,
+name: 
+job:
+text:
+},
+{
+id: 1,
+name: 
+job:
+text:
+
+const author = document.getElementById("author");
+const job = document.getElementById('job");
+const info = document.getElementById("info");
