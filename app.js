56f6c0a2240368aa6225b4ee807936ae25e30a06
pharm2data <93396344+pharm2data@users.noreply.github.com>
2024-04-10 21:26:41 -0400

    update

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const reviews = [
 {
 id:1,
-name:,
-job:,
-text:,
-}
-{
-id: 2
-name:
-job:
-text:
+name:"Joe Smith",
+job:"Gardener",
+text:"Joe likes to garden",
+
+id: 2,
+name:"Mary Smith",
+job:"Flower Planter",
+text:"Mary plants flowers for the greenhouse.",
 }
 ]
 
 const author = document.getElementById("author");
-const job = document.getElementById('job");
+const job = document.getElementById("job");
 const info = document.getElementById("info");
